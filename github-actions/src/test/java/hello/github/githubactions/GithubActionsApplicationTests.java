cb494c7ebf87ae05e0a281206105219755c64e1c
xi_jjun <rlawowns97@naver.com>
2022-08-21 17:57:15 +0900

    project init

---
+++ github-actions/src/test/java/hello/github/githubactions/GithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.github.githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
